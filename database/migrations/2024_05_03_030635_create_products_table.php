1a2f38f8bddbf31aed40a68a9cd3b64444bc2da0
affaiq <thephantomtheif4@gmail.com>
2024-05-03 15:42:32 +0800

    commit

--- database/migrations/2024_05_03_030635_create_products_table.php
+++ database/migrations/2024_05_03_030635_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('details');
+            $table->integer('price');
+            $table->string('publish');
             $table->timestamps();
         });
     }
---
+++ resources/views/admin/product/create.blade.php
@@ -0,0 +1,77 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create Product') }}
+        </h2>
+    </x-slot>
+ 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h1 class="mb-0">Add Product</h1>
+                    <hr />
+                    @if (session()->has('error'))
+                    <div>
+                        {{session('error')}}
+                    </div>
+                    @endif
+                    <p><a href="{{ route('admin/products') }}" class="btn btn-primary">Go Back</a></p>
+ 
+                    <form action="{{ route('admin/products/save') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" name="name" class="form-control" placeholder="Name">
+                                @error('title')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" name="details" class="form-control" placeholder="Details">
+                                @error('category')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <input type="text" name="price" class="form-control" placeholder="Price">
+                                @error('price')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="publish" id="publish_yes" value="1">
+                                    <label class="form-check-label" for="publish_yes">
+                                        Yes
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="publish" id="publish_no" value="0">
+                                    <label class="form-check-label" for="publish_no">
+                                        No
+                                    </label>
+                                </div>
+                                @error('publish')
+                                <span class="text-danger">{{$message}}</span>
+                                @enderror
+                            </div>
+                        </div>
+ 
+                        <div class="row">
+                            <div class="d-grid">
+                                <button class="btn btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
